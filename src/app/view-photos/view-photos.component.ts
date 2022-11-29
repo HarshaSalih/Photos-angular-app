9193c83377d32d490d6962580f4ee2fbb83db966
HarshaSalih <harshasalih2000@gmail.com>
2022-11-29 22:37:30 +0530

    commits

---
+++ src/app/view-photos/view-photos.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-photos',
+  templateUrl: './view-photos.component.html',
+  styleUrls: ['./view-photos.component.css']
+})
+export class ViewPhotosComponent {
+
+
+  constructor(private api:ApiService)
+  {
+    api.fetchPhotos().subscribe(
+      (response)=>{
+        this.data=response;
+      }
+    )
+  }
+
+
+
+  data:any=[]
+
+}
